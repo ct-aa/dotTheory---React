5d5bf9201029a5c94ee896309d99c33767da33d6
Alex Alfred Villanueva <alex.alfred.villanueva@codeandtheory.com>
2023-04-03 11:57:43 +0800

    RCT-285: Data Fetching in React

---
+++ src/components/DataFetchingInReact/DataFetchingInReact.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import "./DataFetchingInReact.css";
+
+const DataFetchingInReact = () => {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1 className="headline">Data Fetching in React</h1>
+      <div className="demo-block">
+        {users &&
+          users.map((user) => (
+            <p className="user" key={user.id}>
+              {user.name}
+            </p>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default DataFetchingInReact;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-logo">
+        <Link exact to="/">
+          Home
+        </Link>
+      </div>
+
+      <ul>
+        <li>
+          <Link to="/what-is-new-in-react">What's New in React</Link>
+        </li>
+        <li>
+          <Link to="data-fetching-in-react">Data Fetching in React</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
